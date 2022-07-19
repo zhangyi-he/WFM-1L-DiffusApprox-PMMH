7e75443f61453a7a91140976d543a9cd3e8c1aef
zhangyi-he <jefferyhe@outlook.com>
2022-07-19 11:31:51 +0100

    Update TEST.R

--- Code v1.3/TEST.R
+++ Code v1.3/TEST.R
@@ -24,7 +24,7 @@ library("plot3D")
 library("emdbook")
 
 # call R functions
-source("./Code v1.3/RFUN.R")
+source("./RFUN.R")
 
 ################################################################################
 
