d26181e7ddb4a7896ca6dbc5f6daff306a8b2f96
zhangyi-he <jefferyhe@outlook.com>
2022-07-19 11:32:01 +0100

    Update RFUN.R

--- Code v1.4/RFUN.R
+++ Code v1.4/RFUN.R
@@ -34,7 +34,7 @@ library("compiler")
 #enableJIT(1)
 
 # call C++ functions
-sourceCpp("./Code v1.4/CFUN.cpp")
+sourceCpp("./CFUN.cpp")
 
 ################################################################################
 
