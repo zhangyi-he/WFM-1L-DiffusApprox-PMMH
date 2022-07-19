ab4545b57f8354bdf51725bc372ee175fcd1e2aa
zhangyi-he <jefferyhe@outlook.com>
2022-07-19 11:31:59 +0100

    Update REAL.R

--- Code v1.4/REAL.R
+++ Code v1.4/REAL.R
@@ -26,7 +26,7 @@ library("plot3D")
 library("emdbook")
 
 # call R functions
-source("./Code v1.4/RFUN.R")
+source("./RFUN.R")
 
 ################################################################################
 
